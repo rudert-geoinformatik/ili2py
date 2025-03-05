282879c1b50987de17f0444be4019796765ea6a9
Clemens Rudert <crud3_rt@posteo.ch>
2025-03-05 18:55:26 +0100

    fixes #8

--- tests/data/prepare_meta_models.sh
+++ tests/data/prepare_meta_models.sh
@@ -6,3 +6,4 @@ ili2c -oIMD16 --out /io/models/Planungszonen_v1_1/Planungszonen_V1_1.imd /io/mod
 ili2c -oIMD16 --out /io/models/SIA405_Abwasser_3D_2015_2_d-20211020/SIA405_Abwasser_3D_2015_2_d-20211020.imd /io/models/SIA405_Abwasser_3D_2015_2_d-20211020/SIA405_Abwasser_3D_2015_2_d-20211020.ili
 ili2c -oIMD16 --out /io/models/SIA405_LKMap_3D_2015_2_d-20180427/SIA405_LKMap_3D_2015_2_d-20180427.imd /io/models/SIA405_LKMap_3D_2015_2_d-20180427/SIA405_LKMap_3D_2015_2_d-20180427.ili
 ili2c -oIMD16 --out /io/models/SO_AFU_ABBAUSTELLEN_Publikation_20221103/SO_AFU_ABBAUSTELLEN_Publikation_20221103.imd /io/models/SO_AFU_ABBAUSTELLEN_Publikation_20221103/SO_AFU_ABBAUSTELLEN_Publikation_20221103.ili
+ili2c -oIMD16 --out /io/models/Nutzungsplanung_V1_2/Nutzungsplanung_V1_2.imd /io/models/Nutzungsplanung_V1_2/Nutzungsplanung_V1_2.ili
