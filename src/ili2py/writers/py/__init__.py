cfbae894bc1dcf774f57d52c7438f9bedd240735
Clemens Rudert <crud3_rt@posteo.ch>
2025-03-17 19:27:19 +0100

    fix

--- src/ili2py/writers/py/__init__.py
+++ src/ili2py/writers/py/__init__.py
@@ -4,8 +4,8 @@ from ili2py.writers.py.interlis23 import reader_classes
 
 
 def create_python_classes(transfer: ImdTransfer, output_path: str) -> str:
-    # TODO: This is probably ugly (aka can we have mixed ili versions?)
-    if transfer.datasection.ModelData[-1].Model.iliVersion == '2.3':
+    # TODO: This is probably ugly (aka can we have mixed ili versions?
+    if transfer.datasection.ModelData[-1].Model.iliVersion in ['2.3', '2.4']:
         return reader_classes(transfer, output_path)
     else:
         raise NotImplemented
