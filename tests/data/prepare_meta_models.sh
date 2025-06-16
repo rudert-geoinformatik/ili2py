4b986c89be180e2f894e7c7ea4e26a42b133e57d
Clemens Rudert <crud3_rt@posteo.ch>
2025-06-16 15:19:00 +0200

    add generated imd models

--- tests/data/prepare_meta_models.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ili2c -oIMD16 --out /io/models/DMAVTYM_Alles_V1_0/DMAVTYM_Alles_V1_0.imd /io/models/DMAVTYM_Alles_V1_0/DMAVTYM_Alles_V1_0.ili
-ili2c -oIMD16 --out /io/models/OeREBKRMtrsfr_V2_0/OeREBKRMtrsfr_V2_0.imd /io/models/OeREBKRMtrsfr_V2_0/OeREBKRMtrsfr_V2_0.ili
-ili2c -oIMD16 --out /io/models/Planungszonen_v1_1/Planungszonen_V1_1.imd /io/models/Planungszonen_v1_1/Planungszonen_V1_1.ili
-ili2c -oIMD16 --out /io/models/SIA405_Abwasser_3D_2015_2_d-20211020/SIA405_Abwasser_3D_2015_2_d-20211020.imd /io/models/SIA405_Abwasser_3D_2015_2_d-20211020/SIA405_Abwasser_3D_2015_2_d-20211020.ili
-ili2c -oIMD16 --out /io/models/SIA405_LKMap_3D_2015_2_d-20180427/SIA405_LKMap_3D_2015_2_d-20180427.imd /io/models/SIA405_LKMap_3D_2015_2_d-20180427/SIA405_LKMap_3D_2015_2_d-20180427.ili
-ili2c -oIMD16 --out /io/models/SO_AFU_ABBAUSTELLEN_Publikation_20221103/SO_AFU_ABBAUSTELLEN_Publikation_20221103.imd /io/models/SO_AFU_ABBAUSTELLEN_Publikation_20221103/SO_AFU_ABBAUSTELLEN_Publikation_20221103.ili
-ili2c -oIMD16 --out /io/models/Nutzungsplanung_V1_2/Nutzungsplanung_V1_2.imd /io/models/Nutzungsplanung_V1_2/Nutzungsplanung_V1_2.ili
